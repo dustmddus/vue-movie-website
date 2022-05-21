0d44c3a98670a26c7a941e920e54146168dadfd6
dustmddus <dustmddus5@gmail.com>
2022-05-21 13:55:59 +0900

    feat : 로딩부분 수정

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -21,13 +21,13 @@ export default {
     onSubmit() {
       this.loading = true;
       this.$store.dispatch("movie/fetchMovie", this.value);
+      this.loading = false;
       this.$router.push({
         name: "MovieList",
         params: {
           query: this.value,
         },
       });
-      // this.loading = false;
     },
   },
   components: {
